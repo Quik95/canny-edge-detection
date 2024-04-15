f7ec400879fc0f3d883f19a622dfdbbc56fb6c57
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2024-04-15 20:56:32 +0200

    Add a lodepng library

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+#include "vendor/lodepng/lodepng.h"
 
 int main() {
-    printf("Hello, World!\n");
+    unsigned error;
+    unsigned char *image;
+    unsigned width, height;
+
+    error = lodepng_decode24_file(&image, &width, &height, "/tmp/test.png");
+    if (error) printf("error %u: %s\n", error, lodepng_error_text(error));
+
+    printf("The loaded image has dimensions %u x %u\n", width, height);
+
+    sleep(20);
+    free(image);
     return 0;
 }
